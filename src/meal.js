c93b1b5caa87d1bdb64de8e19b0b4a5b3ee694a1
Rachel Allen <rallenco13@gmail.com>
2022-03-25 17:48:48 -0600

    completed 3 tests

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,38 @@
-function nameMenuItem() {
-
+function nameMenuItem(item) {
+  var menuItem = `Delicious ${item}`
+  return menuItem
 }
 
+function createMenuItem(itemParam, priceParam, typeParam) {
+  var menuItem = {};
+  menuItem.name = itemParam;
+  menuItem.price = priceParam;
+  menuItem.type = typeParam;
+
+  return menuItem
+};
+
+function addIngredients(ingredient, ingredientsArray) {
+    if (ingredientsArray.includes(ingredient)) {
+      return ingredientsArray
+    } else {
+      return ingredientsArray.push(ingredient)
+    }
+}
+
+function formatPrice(initialPrice) {
+  return `$${initialPrice}`
+}
+
+function decreasePrice(decreasedPrice) {
+  return decreasedPrice * .10
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
   // createRecipe
 }
-
-
