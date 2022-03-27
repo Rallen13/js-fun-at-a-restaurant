a160bf3f30928b8e1372f763450f3fb728de6724
Rachel Allen <rallenco13@gmail.com>
2022-03-27 16:35:43 -0600

    completed restaurant.js

--- src/restaurant.js
+++ src/restaurant.js
@@ -33,8 +33,37 @@ function addMenuItem(restaurantParam, menuItemParam) {
   return restaurantParam
 }
 
+function removeMenuItem(restaurantParam, menuItemParam, menuTypeParam) {
+  var removedItemMessage = `No one is eating our ${menuItemParam} - it has been removed from the ${menuTypeParam} menu!`
+
+  var errorMessage = `Sorry, we don't sell ${menuItemParam}, try adding a new recipe!`
+
+  if (menuTypeParam === "breakfast") {
+    for (var i = 0; i < restaurantParam.menus.breakfast.length; i++) {
+      if (restaurantParam.menus.breakfast[i].name === menuItemParam) {
+        restaurantParam.menus.breakfast.splice(i, 1)
+        return removedItemMessage
+      }
+    }
+  } else if (menuTypeParam === "lunch") {
+    for (var i = 0; i < restaurantParam.menus.lunch.length; i++) {
+      if (restaurantParam.menus.lunch[i].name === menuItemParam) {
+        restaurantParam.menus.lunch.splice(i, 1)
+        return removedItemMessage
+      }
+    }
+  } else if (menuTypeParam === "dinner") {
+    for (var i = 0; i < restaurantParam.menus.dinner.length; i++) {
+      if (restaurantParam.menus.dinner[i].name === menuItemParam) {
+        restaurantParam.menus.dinner.splice(i, 1)
+        return removedItemMessage
+      }
+    }
+  }
+  return errorMessage
+}
 module.exports = {
   createRestaurant,
   addMenuItem,
-  // removeMenuItem
+  removeMenuItem
 }
