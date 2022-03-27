3be557f36ee0ed0bd984cf2bf8f9d56525ba3260
Rachel Allen <rallenco13@gmail.com>
2022-03-27 15:56:46 -0600

    completed addMenuItem

--- src/restaurant.js
+++ src/restaurant.js
@@ -10,10 +10,31 @@ function createRestaurant(nameParam) {
   return restaurant
 }
 
-
+function addMenuItem(restaurantParam, menuItemParam) {
+  if (menuItemParam.type === "breakfast") {
+    if (restaurantParam.menus.breakfast.includes(menuItemParam)) {
+      return restaurantParam
+    } else {
+      restaurantParam.menus.breakfast.push(menuItemParam)
+    }
+  } else if (menuItemParam.type === "lunch") {
+    if (restaurantParam.menus.lunch.includes(menuItemParam)) {
+      return restaurantParam
+    } else {
+      restaurantParam.menus.lunch.push(menuItemParam)
+    }
+  } else if (menuItemParam.type === "dinner") {
+    if (restaurantParam.menus.dinner.includes(menuItemParam)) {
+      return restaurantParam
+    } else {
+      restaurantParam.menus.dinner.push(menuItemParam)
+    }
+  }
+  return restaurantParam
+}
 
 module.exports = {
-   createRestaurant,
-  // addMenuItem,
+  createRestaurant,
+  addMenuItem,
   // removeMenuItem
 }
