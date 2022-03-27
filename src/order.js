b04b64c6866860d19679c48ace6df1bab1086517
Rachel Allen <rallenco13@gmail.com>
2022-03-27 10:13:33 -0600

    completed takeorder and refundorder

--- src/order.js
+++ src/order.js
@@ -1,7 +1,26 @@
+function takeOrder (orderParam, deliveryOrdersArrayParam) {
+  if (deliveryOrdersArrayParam.length < 3) {
+    return deliveryOrdersArrayParam.push(orderParam)
+  } else {
+    return deliveryOrdersArrayParam
+  }
+}
+
+function refundOrder (refundOrderNumberParam, deliveryOrdersArrayParam) {
+  for (var i = 0; i < deliveryOrdersArrayParam.length; i++) {
+    if (deliveryOrdersArrayParam[i].orderNumber === refundOrderNumberParam) {
+      deliveryOrdersArrayParam.splice(i, 1)
+    } else {
+      deliveryOrdersArrayParam
+    }
+  }
+  return
+}
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
+  takeOrder,
+  refundOrder,
   // listItems,
   // searchOrder
 }
